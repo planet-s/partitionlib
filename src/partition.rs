f64415b37b33423d2fe1e633eae1d2e17acadaf8
Deepak Sirone <deepaksirone94@gmail.com>
2019-04-30 23:30:04 +0530

    Add Skeleton

--- src/partition.rs
+++ src/partition.rs
@@ -1,13 +1,37 @@
 use super::Result;
 use std::io::{Read, Write, Seek, SeekFrom};
+use std::path::Path;
+use mbr::partition::read_partitions;
 
 pub struct partition {
     pub start_lba : u64,
-    pub size : u64
+    pub size : u64,
+    pub flags : Option<u64>,
+    pub name : Option<String>,
 }
 
 
-pub fn is_mbr<D: Read + Write + Seek>(disk : &mut D) -> Result<bool> {
-    disk.seek(SeekFrom::Start(0))?;
-    Ok(true)
+pub fn is_mbr(path: &Path) -> bool {
+    let cfg = gpt::GptConfig::new().writable(false);
+    let is_mbr = match cfg.open(path) {
+        Ok(e) => false,
+        Err(x) => true
+    };
+
+    is_mbr
+    //println!("Disk header: {:#?}", disk.primary_header());
+    //println!("Partition layout: {:#?}", disk.partitions());
+
+}
+
+pub fn get_partitions(path: &Path) -> Result<Vec<partition>> {
+    let mut res = Vec::new();
+    if is_mbr(path) {
+        let partitions = read_partitions(path.to_path_buf())?;
+        for part in &partitions {
+            
+        }
+    }
+
+    Ok(res)
 }
