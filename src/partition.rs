6838bd1c09c6aa2b8b3f78fc95365e5530755c9c
Deepak Sirone <deepaksirone94@gmail.com>
2019-05-01 21:15:43 +0530

    Add code

--- src/partition.rs
+++ src/partition.rs
@@ -3,8 +3,12 @@ use std::io::{Read, Write, Seek, SeekFrom};
 use std::path::Path;
 use mbr::partition::read_partitions;
 
+
+#[derive(Clone, Debug)]
 pub struct partition {
+    /// The starting logical block number
     pub start_lba : u64,
+    /// The size of the partition in sectors
     pub size : u64,
     pub flags : Option<u64>,
     pub name : Option<String>,
@@ -29,8 +33,18 @@ pub fn get_partitions(path: &Path) -> Result<Vec<partition>> {
     if is_mbr(path) {
         let partitions = read_partitions(path.to_path_buf())?;
         for part in &partitions {
-            
+            let t = partition {
+                start_lba : part.p_lba as u64,
+                size : part.p_size as u64,
+                flags : None,
+                name : None
+            };
+
+            res.push(t);
         }
+    }
+    else {
+        
     }
 
     Ok(res)
