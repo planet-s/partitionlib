6807374a3b0d3535e18dfa581c2e6f92a132e022
Deepak Sirone <deepaksirone94@gmail.com>
2019-05-15 11:11:31 +0530

    Add extra test

--- src/partition.rs
+++ src/partition.rs
@@ -1,12 +1,13 @@
 use super::Result;
-use std::io::{Read, Write, Seek, SeekFrom};
+//use std::io::{Read, Write, Seek, SeekFrom};
 use std::path::Path;
 use mbr::partition::read_partitions;
 use uuid::Uuid;
 use gpt;
 
+/// A union of the MBR and GPT partition entry
 #[derive(Clone, Debug)]
-pub struct partition {
+pub struct Partition {
     /// The starting logical block number
     pub start_lba : u64,
     /// The size of the partition in sectors
@@ -20,7 +21,7 @@ pub struct partition {
 pub fn is_mbr(path: &Path) -> bool {
     let cfg = gpt::GptConfig::new().writable(false);
     let is_mbr = match cfg.open(path) {
-        Ok(e) => false,
+        Ok(_e) => false,
         Err(x) => {
                     println!("[is_mbr] Error {:?} ", x);
                     true
@@ -33,12 +34,12 @@ pub fn is_mbr(path: &Path) -> bool {
 
 }
 
-pub fn get_partitions(path: &Path) -> Result<Vec<partition>> {
+pub fn get_partitions(path: &Path) -> Result<Vec<Partition>> {
     let mut res = Vec::new();
     if is_mbr(path) {
         let partitions = read_partitions(path.to_path_buf())?;
         for part in &partitions {
-            let t = partition {
+            let t = Partition {
                 start_lba : part.p_lba as u64,
                 size : part.p_size as u64,
                 flags : None,
@@ -52,8 +53,8 @@ pub fn get_partitions(path: &Path) -> Result<Vec<partition>> {
     else {
         let cfg = gpt::GptConfig::new().writable(false);
         let disk = cfg.open(path).expect("failed to open disk");
-        for (id, part) in disk.partitions().iter() {
-            let t = partition {
+        for (_id, part) in disk.partitions().iter() {
+            let t = Partition {
                 start_lba : part.first_lba,
                 size : part.last_lba - part.first_lba + 1,
                 flags : Some(part.flags),
--- tests/test.rs
+++ tests/test.rs
@@ -3,7 +3,13 @@ extern crate partitionlib;
 use partitionlib::{get_partitions, is_mbr};
 
 #[test]
-fn test1() {
+fn test_gpt() {
     let res = is_mbr(std::path::Path::new("./resources/disk.img"));
     assert!(!res);
+}
+
+#[test]
+fn test_mbr() {
+    let res = is_mbr(std::path::Path::new("./resources/disk_mbr.img"));
+    assert!(res);
 }
